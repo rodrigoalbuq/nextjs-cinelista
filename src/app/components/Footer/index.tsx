e7fc4d45777b5e13ffb3cf6e6559d4eb67d256a7
Rodrigo <rodrigoalvalbq@gmail.com>
2026-01-16 20:07:20 -0300

    chore: atualiza README, Footer e adiciona screenshot

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,9 +1,10 @@
 import styles from "./Footer.module.css";
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <p className={styles.footer__text}>
-        &copy; 2025 Cinelista | Desenvolvido por{" "}
+        &copy; {year} Cinelista | Desenvolvido por{" "}
         <a href="https://github.com/rodrigoalbuq">Rodrigo Albuquerque</a> |
         Todos os direitos reservados.
       </p>
